32255be29c119ce53e131853c1ec71600a1ab459
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-23 02:03:36 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -27,8 +27,8 @@ class InteractiveRecord
     self.class.table_name
   end
 
-  def column_name_for_insert 
-
+  def column_name_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}
   end
 
   def initialize(options={})
