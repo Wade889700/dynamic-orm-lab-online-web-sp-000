d6ce561ea8f986a612ecdd0d110ab70e76a89493
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-23 02:07:13 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -8,12 +8,12 @@ class InteractiveRecord
   def self.table_name
          self.to_s.downcase.pluralize
      end
- 
+
       def self.column_names
          DB[:conn].results_as_hash = true
- 
+
           sql = "pragma table_info('#{table_name}')"
- 
+
           table_info = DB[:conn].execute(sql)
          column_names = []
          table_info.each do |row|
@@ -21,20 +21,20 @@ class InteractiveRecord
          end
          column_names.compact
      end
- 
+
       def initialize(options={})
          options.each do |property, value|
              self.send("#{property}=", value)
          end
      end
- 
+
       def table_name_for_insert
          self.class.table_name
      end
- 
+
       def col_names_for_insert
          self.class.column_names.delete_if {|col| col == "id"}.join(", ")
      end
-  
+
 
 end
