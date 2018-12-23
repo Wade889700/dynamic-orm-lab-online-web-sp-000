0410f4f6e253fa1cf98b83c2b8ff6695c15e8c7e
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-23 02:21:41 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -43,10 +43,12 @@ class InteractiveRecord
     values << "'#{send(col_name)}'" unless send(col_name).nil?
   end
   values.join(", ")
-end
+ end
 
-  def save
-
+ def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
   def self.find_by_name
